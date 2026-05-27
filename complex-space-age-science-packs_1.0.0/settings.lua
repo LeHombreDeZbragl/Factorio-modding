59f90bac4f7b102d51c1f9ad42cf3b76af3b4ade
LeHombreDeZbragl <tombuchta8@gmail.com>
2026-05-27 15:35:04 +0200

    Feat: revert the metallurgic pack drag

--- complex-space-age-science-packs_1.0.0/settings.lua
+++ complex-space-age-science-packs_1.0.0/settings.lua
@@ -11,23 +11,5 @@ data:extend({
         setting_type = "startup",
         default_value = 1000,
         minimum_value = 1
-    },
-    {
-        type = "int-setting",
-        name = "ga-metallurgic-weight",
-        setting_type = "startup",
-        default_value = 5,
-        minimum_value = 1,
-        maximum_value = 1000,
-        order = "c"
-    },
-    {
-        type = "int-setting",
-        name = "ga-metallurgic-platform-drag-weight",
-        setting_type = "startup",
-        default_value = 5000,
-        minimum_value = 1,
-        maximum_value = 100000,
-        order = "d"
     }
 })
